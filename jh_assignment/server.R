0aa89b4ffc4fca97e64296c0d3ca0fae4d67557d
Anatoly Vasilyev <anatoly.developer@gmail.com>
2016-09-11 16:17:17 +0300

    almost finished with app

--- jh_assignment/server.R
+++ jh_assignment/server.R
@@ -1,17 +1,28 @@
 library(shiny)
 library(data.table)
 library(ggplot2)
+library(gtools)
+library(caret)
 
 data.nei.baltimore <- fread('data/data_nei_baltimore.csv')
+data.nei.baltimore$V1 <- NULL
+data.nei.baltimore$fips <- NULL
+data.nei.baltimore$SCC <- NULL
+data.nei.baltimore$Pollutant <- NULL
 data.nei.baltimore[,summ:=sum(Emissions), by=list(year, type)]
+data.nei.baltimore$year <- as.numeric(data.nei.baltimore$year)
 
 shinyServer(function(input, output) {
   #' TODO: 
   #' 1. handle empty data error
   #' 2. add some more user input - probably other cities, or calculate some value like sum of something
   #' 3. explain types in GUI
+  #' 4. use some bigger city? to have more data
   
   output$emissions.plot <- renderPlot({
+    if(invalid(input$type)){
+      return()
+    } 
     data.sum <- data.nei.baltimore[type %in% input$type, sum(Emissions), by=list(year)]
     setnames(data.sum, c('year', 'emissions'))
     
@@ -19,16 +30,17 @@ shinyServer(function(input, output) {
     ggplot(data.sum, aes(year, emissions, group=1)) + 
       geom_point() + 
       geom_line() + 
-      ylab("Emissions summ") + 
-      ggtitle("emissions")
-    
-    # generate bins based on input$bins from ui.R
-    # x    <- faithful[, 2] 
-    # bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    # hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+      ylab("Amount of PM2.5 emitted, tons") + 
+      ggtitle("Baltimore PM2.5 emissions")
   })
-  
+  prediction <- eventReactive(input$submit.button, {
+    data.sum <- data.nei.baltimore[type %in% input$type, sum(Emissions), by=list(year)]
+    setnames(data.sum, c('year', 'emissions'))
+    model <- lm(emissions ~ year, data.sum)
+    result <- predict(model, data.table(year=input$year))
+  })
+  output$emissions.predict <- renderText({
+    result <- prediction()
+    paste(result)
+  })
 })
--- jh_assignment/ui.R
+++ jh_assignment/ui.R
@@ -2,26 +2,33 @@ library(shiny)
 library(data.table)
 
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      checkboxGroupInput("type", 
-                         label = h3("Checkbox group"), 
-                         choices = list("NONPOINT", 
-                                        "POINT",
-                                        "NON-ROAD", 
-                                        "ON-ROAD"),
-                         selected = "NONPOINT")
+  title = "Diamonds Explorer",
+  fluidRow(
+    column(5,
+           h4("Diamonds Explorer"),
+           checkboxGroupInput("type", 
+                              label = h4("Emissions source types to plot and to use with predict function"), 
+                              choices = list("NONPOINT", 
+                                             "POINT",
+                                             "NON-ROAD", 
+                                             "ON-ROAD"),
+                              selected = "NONPOINT")
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("emissions.plot")
+    column(7,
+           plotOutput("emissions.plot")
+    )
+  ),
+  hr(),
+  fluidRow(
+    column(5,
+           numericInput(inputId="year", 
+                        label = h4("A year for which predict amount of PM2.5"),
+                        value = 2010, min = 2009, max=2030, step = 1),
+           actionButton("submit.button","Submit")
+    ),
+    column(7,
+           h4('Predicted amount of PM2.5 emitted for specified year and sources'),
+           verbatimTextOutput('emissions.predict')
     )
   )
 ))
